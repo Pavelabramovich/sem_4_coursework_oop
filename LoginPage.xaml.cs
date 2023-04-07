1a8aad9513d7122610adcb5ee01893e744eae226
Pavelabramovich <apn20040510@gmail.com>
2023-04-08 00:52:42 +0300

    add unmasked password feather

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -26,6 +26,21 @@ public partial class LoginPage : Page
 
     private UserDb _userDb;
 
+    private string Password
+    {
+        get
+        {
+            if (PasswordHidden.Visibility == Visibility.Visible)
+            {
+                return PasswordHidden.Password;
+            }
+            else
+            {
+                return PasswordUnmask.Text;
+            }
+        }
+    }
+
     public LoginPage(MainWindow mainWindow)
     {
         InitializeComponent();
@@ -37,14 +52,71 @@ public partial class LoginPage : Page
     private void Button_Click_1(object sender, RoutedEventArgs e)
     {
         string login = loginTextBox.Text;
-        string password = passwordTextBox.Password;
+        string password = Password;
 
         if (_userDb.Users.FirstOrDefault(u => u.Login == login && u.Password == password) != null)
+        {
             _mainWindow.OpenSecondPage();
+        }
+        else
+        {
+            invalidLoginLabel.Content = "Invalid login";
+            invalidPasswordLabel.Content = "Invalid password";
+        }
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
         _mainWindow.OpenSecondPage();
+    }
+
+    private void ClearLabels()
+    {
+        invalidLoginLabel.Content = string.Empty;
+        invalidPasswordLabel.Content = string.Empty;
+    }
+    //private void ShowPassword_PreviewMouseDown(object sender, MouseButtonEventArgs e) => ShowPasswordFunction();
+    //private void ShowPassword_PreviewMouseUp(object sender, MouseButtonEventArgs e) => HidePasswordFunction();
+    //private void ShowPassword_MouseLeave(object sender, MouseEventArgs e) => HidePasswordFunction();
+
+    private void ShowPasswordFunction()
+    {
+        //ShowPassword.Text = "HIDE";
+        PasswordUnmask.Visibility = Visibility.Visible;
+        PasswordHidden.Visibility = Visibility.Collapsed;
+
+        if (PasswordHidden.Password != PasswordUnmask.Text)
+            PasswordUnmask.Text = PasswordHidden.Password;
+    }
+
+    private void HidePasswordFunction()
+    {
+       // ShowPassword.Text = "SHOW";
+        PasswordUnmask.Visibility = Visibility.Collapsed;
+        PasswordHidden.Visibility = Visibility.Visible;
+
+        if (PasswordHidden.Password != PasswordUnmask.Text)
+            PasswordHidden.Password = PasswordUnmask.Text;
+    }
+
+    private void hidePasswordCheckBox_Checked(object sender, RoutedEventArgs e)
+    {
+        ShowPasswordFunction();
+    }
+
+    private void hidePasswordCheckBox_Unchecked(object sender, RoutedEventArgs e)
+    {
+       
+        HidePasswordFunction();
+    }
+
+    private void PasswordHidden_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        ClearLabels();
+    }
+
+    private void PasswordUnmask_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        ClearLabels();
     }
 }
